337a9906ca0799f2e1161f3e0f2af1627b90bccc
James Hodgson <james.hodgson@haemonetics.com>
2015-06-18 15:08:55 -0600

    - Updated the pegdown version to 1.5.0 - Added a extra case in the MDtoHTMLExpLinkRenderer to work better with relative links that also have anchors. This is not a completely robust solution but it works for the bulk of the cases. - Replaced a few more "/" with file.separator - Added anchorlinks to the list of pegdown extensions. This was added in 1.5.0 of pegodown

--- src/main/java/com/ruleoftech/markdown/page/generator/plugin/MDToHTMLExpLinkRender.java
+++ src/main/java/com/ruleoftech/markdown/page/generator/plugin/MDToHTMLExpLinkRender.java
@@ -11,11 +11,14 @@ public class MDToHTMLExpLinkRender extends LinkRenderer {
     @Override
     public Rendering render(ExpLinkNode node, String text) {
         String url = node.url;
-        if (!url.startsWith("http://") && !url.startsWith("https://") && url.endsWith(".md")) {
-            url = url.substring(0, url.length() - 2) + "html";
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            if(url.endsWith(".md")){
+                url = url.substring(0, url.length() - 2) + "html";
+            }else if(url.contains(".md#")){
+                url = url.replace(".md#", ".html#");
+            }
         }
         Rendering rendering = new Rendering(url, text);
         return StringUtils.isEmpty(node.title) ? rendering : rendering.withAttribute("title", encode(node.title));
     }
-
 }
