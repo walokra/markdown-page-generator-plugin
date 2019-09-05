e8279e59e86503edee9f716549bb9f784b73d5bb
Guido Forthofer <guido.forthofer@boschrexroth.de>
2019-09-05 06:52:21 +0200

    Fix: Problems inside maven multimodule or aggregator project
    pom.xml     (parent) -> Declaration this maven plugin
      -- pom.xml  (Child A needs this plugin)
      -- pom.xml  (Child B needs not this plugin)  <-- Error
      -- pom.xml  (Child C needs this plugin)

--- src/main/java/com/ruleoftech/markdown/page/generator/plugin/MdPageGeneratorMojo.java
+++ src/main/java/com/ruleoftech/markdown/page/generator/plugin/MdPageGeneratorMojo.java
@@ -205,7 +205,10 @@ public class MdPageGeneratorMojo extends AbstractMojo {
         }
 
         getLog().info("Pre-processing markdown files from input directory: " + inputDirectory);
-        preprocessMarkdownFiles(new File(inputDirectory));
+        if (!preprocessMarkdownFiles(new File(inputDirectory))){
+			getLog().info("Pre-processing markdown files from input directory: markdown files not found" + inputDirectory);
+			return;
+		}
 
         if (!markdownDTOs.isEmpty()) {
             getLog().info("Process Pegdown extension options");
